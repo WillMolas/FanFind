e45a6c6cf78a8824d24e64851e97551807c8178c
Guillem Molas <gmolas1@gmail.com>
2022-11-29 16:31:17 +0100

    added some stuff

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,12 +1,12 @@
 class MatchesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: :index
   def index
     @matches = Match.all
     @matches = Match.order(match_time: :desc)
 
     if params[:query].present?
       sql_query = 'team_one ILIKE :query OR team_two ILIKE :query'
-      @movies = @movies.where(sql_query, query: "%#{params[:query]}%")
+      @matches = @matches.where(sql_query, query: "%#{params[:query]}%")
     end
 
     respond_to do |format|
--- app/javascript/controllers/search_matches_controller.js
+++ app/javascript/controllers/search_matches_controller.js
@@ -5,9 +5,10 @@ export default class extends Controller {
   static targets = ["form", "input", "list"]
 
   connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
+    console.log(this.element);
+    console.log(this.formTarget);
+    console.log(this.inputTarget);
+    console.log(this.listTarget);
   }
 
   update() {
