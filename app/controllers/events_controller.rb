551ee6ade0927afb81d6a98a3906e21535dd1d74
Jess Pates <jesspates@hotmail.co.uk>
2022-11-29 12:38:51 +0100

    index page for events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,12 @@
+class EventsController < ApplicationController
+  # def index
+  #   @match = Match.find(params[:match_id])
+  #   @match.events = @events
+  # end
+
+  # private
+
+  # def event_params
+  #   params.require(:event).permit(:name, :description, :date_time)
+  # end
+end
