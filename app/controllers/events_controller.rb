ad27ff3dfe10cca0d650ba50cade99b2d035a71b
Jess Pates <jesspates@hotmail.co.uk>
2022-11-29 17:33:50 +0100

    basic outline of event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,8 @@ class EventsController < ApplicationController
     @match = Match.find(params[:match_id])
     @events = @match.events
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
 end
