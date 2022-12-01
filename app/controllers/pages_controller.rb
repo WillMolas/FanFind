43aeb3835d8d185295e7326925838f67f59bda85
Emilia Rojo <emiliamrojo@gmail.com>
2022-12-01 15:18:00 +0100

    Finished stimulus controller for signup and updated DB

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      redirect_to matches_path(@matches)
+    end
   end
 end
