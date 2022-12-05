abf5100b6704753e3498a0afc43375817209595e
Guillem Molas <gmolas1@gmail.com>
2022-12-05 15:13:37 +0100

    finished postcomments notifications

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,13 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  # belongs_to :recipient, class_name: "User"
+
+  after_create_commit :notify_post_creator
+
+
+  def notify_post_creator
+    CommentNotification.with(post: self.post).deliver(self.post.user)
+  end
+
 end
