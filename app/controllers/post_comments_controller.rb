e04aecc7b7ec05be0b65bc5417a904d0612d5950
Jess Pates <jesspates@hotmail.co.uk>
2022-12-05 15:26:34 +0100

    post comments fixed

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class PostCommentsController < ApplicationController
   def index
-    @post = Post.find(params[:match_id])
+    @post = Post.find(params[:post_id])
     @post_comments = @post.post_comments
-  end
-
-  def show
-    @post_comments = PostComment.find(params[:id])
     @post_comment = PostComment.new
   end
 
+  # def show
+  #   @post_comments = PostComment.find(params[:id])
+  #   @post_comment = PostComment.new
+  # end
+
   def new
-    @post = Post.find(params[:match_id])
+    @post = Post.find(params[:post_id])
     @post_comment = PostComment.new
   end
 
@@ -21,18 +22,18 @@ class PostCommentsController < ApplicationController
     @post_comment.user = current_user
 
     if @post_comment.save
-      redirect_to @post_comment.post, notice: "Comment was successfully created."
+      redirect_to @post_comment.post
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def update
-    @post_comment = PostComment.find(params[:id])
-    @post_comment.update(post_comment_params)
-    @post_comment = @post_comment.post
-    redirect_to post_comment_path
-  end
+  # def update
+  #   @post_comment = PostComment.find(params[:id])
+  #   @post_comment.update(post_comment_params)
+  #   @post_comment = @post_comment.post
+  #   redirect_to post_comment_path
+  # end
 
   def destroy
     @post_comment = Event.find(params[:id])
