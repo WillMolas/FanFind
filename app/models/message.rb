34f1dca137856fe5cef5c6b0b21e4ff54f359779
Guillem Molas <gmolas1@gmail.com>
2022-12-06 16:26:42 +0100

    added messages notifications

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,10 @@ class Message < ApplicationRecord
   belongs_to :user
 
   has_one_attached :avatar
+  after_create_commit :notify_message_recipient
+
+
+  def notify_message_recipient
+    NewMessage.with(chatroom: self.chatroom).deliver(self.chatroom.event.users)
+  end
 end
