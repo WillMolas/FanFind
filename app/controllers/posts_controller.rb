72638738110b712ae85e9a8d0a1996adc3ba54fe
Emilia Rojo <emiliamrojo@gmail.com>
2022-12-07 12:48:33 +0100

    fixed og metatags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @posts = Post.order('updated_at DESC')
-    set_meta_tags title: 'Community'
+    set_meta_tags title: 'Community',
+    description: 'Watch upcoming FIFA World Cup matches with like-minded fans who support your team.
+    Find events near you or create your own.',
+    keywords: 'world cup, FIFA, matches, fans, soccer, football,
+    soccer fans, football fans, events, meet up, find fans,
+    team, football team',
+    og: {
+      description: 'Team up to watch the FIFA World Cup. Find events near you or create your own,
+      chat with other fans, and share posts.',
+      type: 'mobile app',
+      url: 'www.fan-find.com',
+      image: 'metatag-photo.png'
+    }
   end
 
   def new
